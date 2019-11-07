15a44deb41881955e468b4d5bbf818bed721ec9d
jerry <17481452@qq.com>
2019-11-07 09:10:00 +0800

    由学院发起的项目终止流程

--- tm-hunt-api/grails-app/controllers/cn/edu/bnuz/bell/hunt/ProjectDepartmentController.groovy
+++ tm-hunt-api/grails-app/controllers/cn/edu/bnuz/bell/hunt/ProjectDepartmentController.groovy
@@ -1,11 +1,15 @@
 package cn.edu.bnuz.bell.hunt
 
+import cn.edu.bnuz.bell.hunt.cmd.InfoChangeCommand
 import cn.edu.bnuz.bell.hunt.cmd.ProjectDepartmentOptionCommand
+import cn.edu.bnuz.bell.workflow.commands.SubmitCommand
 import org.springframework.security.access.prepost.PreAuthorize
 
 @PreAuthorize('hasAuthority("PERM_HUNT_CHECK")')
 class ProjectDepartmentController {
     ProjectDepartmentService projectDepartmentService
+    InfoChangeService infoChangeService
+    ProjectReviewerService projectReviewerService
 
     def index(String checkerId, ProjectDepartmentOptionCommand cmd) {
         renderJson projectDepartmentService.list(cmd)
@@ -13,6 +17,18 @@ class ProjectDepartmentController {
 
     def show(String checkerId, Long id) {
         renderJson projectDepartmentService.getFormInfo(id)
+    }
+
+    def save(String checkerId) {
+        def cmd = new InfoChangeCommand()
+        bindData(cmd, request.JSON)
+        def form = infoChangeService.create(cmd)
+        def smtcmd = new SubmitCommand()
+        smtcmd.id = form.id
+        smtcmd.title = cmd.title
+        smtcmd.to = projectReviewerService.getApprovers()[0]?.id
+        infoChangeService.submit(checkerId, smtcmd)
+        renderJson([id: form.id])
     }
 
 }
